ac255c3a5707f458c91a4a943c8b4c51d89b3c48
venusat <36872247+venusat@users.noreply.github.com>
2019-08-03 20:43:30 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+
+namespace ProgressBar
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            Console.SetCursorPosition(1, 1);
+
+            for (int i = 0; i <= 20; i++)
+            {
+
+                for (int y = 0; y < i; y++)
+                {
+                    Console.Write("x");
+                }
+
+                Console.Write(i + "/20");
+                Console.SetCursorPosition(1, 1);
+                Thread.Sleep(100);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
